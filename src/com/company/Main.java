5c1ccebf6b04b2ed6bb2eb5bc94f3bb3a5b257f5
wxbpxm <wxbpxm@163.com>
2021-03-07 22:55:24 +0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.err.println("SSSSSSS");
+        //TODO adfadsf
+    }
+    public static void main2(String[] args) {
+	// write your code here
+
+    }
+}
---
+++ src/com/company/Test.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import com.sun.xml.internal.bind.v2.TODO;
+
+/**
+ * @author wangxuebiao
+ * @date 2021/3/4 - 20:57
+ * asdfdasf撒地方
+ */
+public class Test {
+    String ss = new String("ss");
+//    TODO aaa
+}
